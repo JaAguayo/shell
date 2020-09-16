c126d692ce4ab0ecdff4a1f71c6ce0c2df746700
JaAguayo <jmaguayo@miners.utep.edu>
2020-09-15 22:04:19 -0600

    fixed redirect, fixed pipe, fixed cd to work with test, failing 2 commands and background

--- shell/newShell.py
+++ shell/newShell.py
@@ -15,9 +15,30 @@ def execute_command(command):
             pass
     os.write(2, ("Command %s not found\n" % command[0]).encode())
 
-def output_redir(command):
-    command = command.split()
 
+def input_redir(command):
+    
+    rc = os.fork()
+    
+    if rc < 0:
+        os.write(2,"Fork failed".encode())
+        sys.exit(1)
+        
+    elif rc == 0:
+        os.close(0)
+        os.open(command[-1],os.O_RDONLY)
+        os.set_inheritable(0,True)
+
+        command = command[0:command.index("<")]
+        execute_command(command)
+
+        os.write(2, ("Command %s not found\n" % command[0]).encode())
+        sys.exit(1)
+
+
+    
+def output_redir(command):
+    
     rc = os.fork()
 
     if rc < 0:
@@ -35,8 +56,8 @@ def output_redir(command):
         os.write(2, ("Command %s not found\n" % command[0]).encode())
         sys.exit(1)
 
+       
 def pipe(command):
-    command = command.split()
     i = command.index('|')
     pipe1 = command[0:i]
     pipe2 = command[i+1:len(command)]
@@ -58,7 +79,7 @@ def pipe(command):
         for f in (r,w):
             os.close(f)
             
-        command = pipe1
+        execute_command(pipe1)
 
     else:
         os.close(0)
@@ -67,14 +88,19 @@ def pipe(command):
         for f in (r,w):
             os.close(f)
 
-        command = pipe2
+        execute_command(pipe2)
         
-    execute_command(command)
 
 while(1):
     current_dir = os.getcwd()
-    
-    command = input('$ ')
+    prompt = '$ '
+    if 'PS1' in os.environ:
+        prompt = os.environ['PS1']
+
+    try:
+        command = [str(i) for i in input(prompt).split()]
+    except EOFError:
+        sys.exit(1)
 
     if command == '':
         continue
@@ -86,16 +112,12 @@ while(1):
         os.write(1,("help was selected\n").encode())
 
     elif 'echo' in command:
-        command = command.split(' ')
         os.write(1,(command[1] + "\n").encode())
         
     elif command == 'ls':
         dirs = os.listdir(current_dir)
-        print('File List')
-        print('----------------')
         for file in dirs:
-            print(file)
-        print('----------------')
+            os.write(1,(file + "\n").encode())
     
     elif 'cd' in command:
         if '..' in command:
@@ -103,11 +125,8 @@ while(1):
         else:
             change = command.split('cd')[1].strip()
         try:
-            print(current_dir)
             os.chdir(change)
-            current_dir = os.getcwd()
-            print(current_dir)
-            
+                        
         except FileNotFoundError:
             os.write(2,("cd: no such file or directory\n").encode())
 
@@ -117,14 +136,21 @@ while(1):
     elif '>' in command:
         output_redir(command)
 
+    elif '<' in command:
+        input_redir(command)
+
     elif '|' in command:
         pipe(command)
         
-
     elif '&' in command:
         background = True
         print(background)
 
+    elif '/'in command[0]:
+        program = command[0]
+        try:
+            os.execve(program,command,os.environ)
+        except FileNotFoundError:
+            pass
     else:
-        command = command.split()
-        os.write(2,("%s: command not found\n" % command[0]).encode())
+        execute_command(command)
