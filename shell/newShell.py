75840eb2c3e4e79b0f73e444832840046b487a07
JaAguayo <jmaguayo@miners.utep.edu>
2020-09-15 21:04:49 -0600

    added pipe, need to add background tasks

--- shell/newShell.py
+++ shell/newShell.py
@@ -13,79 +13,63 @@ def execute_command(command):
             pass
         except ValueError:
             pass
-
-def execute_path(command):
-    try:
-        os.execve(command[0],command,os.environ)
-    except FileNotFoundError:
-        pass
-
-def execute(command):
-    rc = os.fork()
-    if rc < 0:
-        os.write(2,"Fork failed".encode())
-        sys.exit(1)
-    elif rc == 0:
-        command = [i.strip() for i in re.split('',command)]
-        if '/' in command [0]:
-            execute_path(command)
-        else:
-            execute_command(command)
-            os.write(2, ("Command %s not found\n" % command[0]).encode())
-            sys.exit(1)
-    else:
-        child_pid_code = os.wait()
+    os.write(2, ("Command %s not found\n" % command[0]).encode())
 
 def output_redir(command):
-    command,file_path = [i.strip() for i in re.split('>',command)]
-    file_path = os.getcwd() + '/' + file_path
-    print(file_path)
-    command = [i.strip() for i in re.split(' ',command)]
-    print(command)
+    command = command.split()
+
     rc = os.fork()
+
     if rc < 0:
         os.write(2,"Fork failed".encode())
         sys.exit(1)
         
     elif rc == 0:
         os.close(1)
-        os.open(file_path,os.O_CREAT | os.O_WRONLY);
+        os.open(command[-1],os.O_CREAT | os.O_WRONLY)
         os.set_inheritable(1,True)
-        
-        execute_command(command)
-        
-        os.write(2, ("Command %s not found\n" % command[0]).encode())
-        sys.exit(1)
-    else:
-        child_pid_code = os.wait()
 
-def input_redir(command):
-    command,file_path = [i.strip() for i in re.split('>',command)]
-    file_path = os.getcwd() + '/' + file_path
-    print(file_path)
-    command = [i.strip() for i in re.split(' ',command)]
-    print(command)
-    rc = os.fork()
-    if rc < 0:
-        os.write(2,"Fork failed".encode())
-        sys.exit(1)
-        
-    elif rc == 0:
-        os.close(0)
-        os.open(file_path,os.O_RDONLY);
-        os.set_inheritable(0,True)
-        
+        command = command[0:command.index(">")]
         execute_command(command)
-        
+
         os.write(2, ("Command %s not found\n" % command[0]).encode())
         sys.exit(1)
-    else:
-        child_pid_code = os.wait()
 
 def pipe(command):
+    command = command.split()
+    i = command.index('|')
+    pipe1 = command[0:i]
+    pipe2 = command[i+1:len(command)]
+
     r,w = os.pipe()
+
     for f in (r,w):
         os.set_inheritable(f,True)
+    pipeChild = os.fork()
+
+    if pipeChild < 0:
+        os.write(2,"Fork failed".encode())
+        sys.exit(1)
+
+    if pipeChild == 0:
+        os.close(1)
+        os.dup(w)
+        os.set_inheritable(1,True)
+        for f in (r,w):
+            os.close(f)
+            
+        command = pipe1
+
+    else:
+        os.close(0)
+        os.dup(r)
+        os.set_inheritable(0,True)
+        for f in (r,w):
+            os.close(f)
+
+        command = pipe2
+        
+    execute_command(command)
 
 while(1):
     current_dir = os.getcwd()
@@ -100,6 +84,10 @@ while(1):
         
     elif command == 'help':
         os.write(1,("help was selected\n").encode())
+
+    elif 'echo' in command:
+        command = command.split(' ')
+        os.write(1,(command[1] + "\n").encode())
         
     elif command == 'ls':
         dirs = os.listdir(current_dir)
@@ -129,12 +117,9 @@ while(1):
     elif '>' in command:
         output_redir(command)
 
-    elif '<' in command:
-        input_redir(command)
-
     elif '|' in command:
-        pipe = True
-        print(pipe)
+        pipe(command)
+        
 
     elif '&' in command:
         background = True
